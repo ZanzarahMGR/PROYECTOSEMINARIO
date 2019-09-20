082164e3d25dda72fd8b3f48c8c33c84900861e6
Ditmar <ditmarcastro@gmail.com>
2019-09-20 10:00:35 -0400

    changes

--- app.js
+++ app.js
@@ -37,5 +37,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+var port = 8000;
+app.listen(port, () => {
+  console.log("Corriendo " + port);
+});
 module.exports = app;
